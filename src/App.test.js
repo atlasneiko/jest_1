40b1eb4cae3b38f781ef8b220918f3cb0b9d3f2f
AtlasNeiko <atlasneiko@gmail.com>
2021-05-31 10:41:52 -0400

    setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+test('renders wihtout error', () => {
+  const wrapper = shallow(<App />);
 });
+test('renders increment button', () => {});
+test('renders counter display', () => {});
+test('counter dispaly starts with 0', () => {});
+test('clicking button increments counter display', () => {});
